cf4f55bcd901a426af7a9b20ea81f8ec93aa38c1
marknikolic <marko.nikolic@unicreditgroup.rs>
2025-07-13 14:09:11 +0200

    nital commit

---
+++ src/test/java/com/marko/CrudRestJPADemo/CrudRestJpaDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marko.CrudRestJPADemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudRestJpaDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
